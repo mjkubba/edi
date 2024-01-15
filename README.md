b674a52b74efd4cc06b343707830fe900173d6ff
MJ Kubba <mjkubba@gmail.com>
2024-01-14 19:30:13 -0600

    adding 2nd nm1

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ## Purpose
 Playground to read EDI files, starting with 835 because why not!   
-First iteration is to read the the file contents and parse it's components, get famililar with the loops and segments then phase 2 write the file itself from other sources
+I'm not new to MMIS systems but wanted to get deeper in the claims processing part, and what a better way to learn a system than building parts of it!
+First iteration is to read the the file contents and parse it's components, get famililar with the loops and segments Then phase 2 write the file itself from other sources
 
+I'm writing this in rust but in python style, as I'm still learning rust!
+
+I'm using Amazon CodeWhisperer to help me with this new endeavour if there are any code references I'll include them here.
 
 ### Artifacts and Components:
 Used example file from https://x12.org/examples/005010x221 located at the src dir, will not be included in the repo (gitignored)
--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,18 @@ fn main() {
     let mut file = File::open("./src/edi835-1.edi").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
+
+    // find how many nm1 segments are in the file
+    let nm1_count = contents.matches("NM1").count();
+    println!("Number of NM1 segments: {}", nm1_count);
     
-    // 
-    let nanme_index = contents.find("NM1").unwrap();
-    let name_start = &contents[nanme_index..];
+    // find the first occurrence of "NM1" in the contents of the file and extract the content between "NM1" and "~"
+    let name_index = contents.find("NM1").unwrap();
+    let name_start = &contents[name_index..];
     let name_end = name_start.find("~").unwrap();
     let name_content = &name_start[4..name_end];
-        
+
+    // Split the name content into parts and set new vars for each segment
     let nm1_parts: Vec<&str> = name_content.split("*").collect();
     let nm101_entity_id = nm1_parts[0];
     let nm102_entity_type = nm1_parts[1];
@@ -25,6 +30,8 @@ fn main() {
     let nm108_id_code = nm1_parts[7];
     let nm109_member_number = nm1_parts[8];
     
+    // Print the extracted content
+    println!("First NM1 section");
     println!("Entity ID: {}", nm101_entity_id);
     println!("Entity Type: {}", nm102_entity_type);
     println!("Last Name: {}", nm103_lastname);
@@ -35,9 +42,35 @@ fn main() {
     println!("ID Code: {}", nm108_id_code);
     println!("Member Number: {}", nm109_member_number);
 
+    // find the second occurrence of "NM1" in the contents of the file and extract the content between "NM1" and "~"
+    let other_nm1 = &name_start[name_end+1..];
+    println!("{:?}", other_nm1);
+    let provider_end = other_nm1.find("~").unwrap();
+    let provider_content = &other_nm1[4..provider_end];
+    println!("{:?}", provider_content);
 
+    // Split the provider content into parts and set new vars for each segment
+    let provider_parts: Vec<&str> = provider_content.split("*").collect();
+    let nm101_provider_entity_id = provider_parts[0];
+    let nm102_provider_entity_type = provider_parts[1];
+    let nm103_provider_lastname = provider_parts[2];
+    let nm104_provider_firstname = provider_parts[3];
+    let nm105_provider_middle_initial = provider_parts[4];
+    let nm106_provider_prefix = provider_parts[5];
+    let nm107_provider_suffix = provider_parts[6];
+    let nm108_provider_id_code = provider_parts[7];
+    let nm109_provider_nat_identifier = provider_parts[8];
 
-
+    // print the other nm1 content
+    println!("Second NM1 section");
+    println!("Entity ID: {}", nm101_provider_entity_id);
+    println!("Entity Type: {}", nm102_provider_entity_type);
+    println!("Last Name: {}", nm103_provider_lastname);
+    println!("First Name: {}", nm104_provider_firstname);
+    println!("Middle Initial: {}", nm105_provider_middle_initial);
+    println!("Prefix: {}", nm106_provider_prefix);
+    println!("Suffix: {}", nm107_provider_suffix);
+    println!("ID Code: {}", nm108_provider_id_code);
+    println!("National Identifier: {}", nm109_provider_nat_identifier);
     
-
 }
