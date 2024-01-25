ff8e2f267ed87af42974710888fbb35cd6d75ee7
MJ Kubba <mjkubba@gmail.com>
2024-01-24 19:19:38 -0600

    some cleaning

--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,8 @@ fn main() {
 
     /*
     TODO:
-        2 ideas:
-        if the assumption of order is correct then we can trim the contents as we go.
-        the 2nd idea safer and need to "extract" the data from the overall string then merge the parts back to one string
-        yup idea 1 didn't work since there might be some loops I didn't count for so the trim start function is not finding
-        the passed in string at the start.
-        ok that was easy, use replace(string, "")
-
-        Figure out the looping of the segments,
-        Will ask EDI SMEs what is the best way to figure this out, my first idea is to count the elements and see how many 
-        of a specific unique segment in specific loop is coming required is preferred but some loops are all situational, eg svc in loop 2110.
-        loop 1000 A and B don't repeat
-        loop 2000 have no required segments but LX is there in all my test files
-        loop 2100 can use CLP
-        loop 2110 have no required segments but can use SVC 
-        if no SVC after MN1 in the previous loop '~' and this new loop have SE then it's table 3
-        if there's something else not SE then we are in either another loop of 2100 (check for CLP) 
-        if all this fails then SVC is not a good indicator
-
+        implement logger
+        make it safer when something does not exist
         println statement is in helper.rs line 3
     */
 
@@ -57,9 +41,7 @@ fn main() {
     let lx_count= contents.matches("LX").count();
     let mut loop_2000_array = vec![];
     println!("Number of loops in loop 2000: {:?}",lx_count);
-    let mut lx;
-    let mut ts2;
-    let mut ts3;
+    let (mut lx, mut ts3, mut ts2);
 
     for _ in 0..lx_count {
         (lx, ts3, ts2, contents) = get_loop_2000(contents.clone());
@@ -73,25 +55,9 @@ fn main() {
     let clp_count= contents.matches("CLP").count();
     let mut loop_2100_array = vec![];
     println!("Number of loops in loop 2100: {:?}",clp_count);
-    let mut clp;
-    let mut claim_adjustment;
-    let mut nm1_patient;
-    let mut nm1_insured;
-    let mut nm1_corrected_patient;
-    let mut nm1_service_provider;
-    let mut nm1_crossover_carrier;
-    let mut nm1_corrected_priority_payer;
-    let mut nm1_other_subscriber;
-    let mut mia;
-    let mut moa;
-    let mut ref_other_claim;
-    let mut ref_rendering_provider;
-    let mut dtm_statement_from;
-    let mut dtm_coverage_expiration;
-    let mut dtm_claim_received;
-    let mut per;
-    let mut amt;
-    let mut qty;
+    let (mut clp, mut claim_adjustment, mut nm1_patient, mut nm1_insured, mut nm1_corrected_patient, mut nm1_service_provider, 
+         mut nm1_crossover_carrier, mut nm1_corrected_priority_payer, mut nm1_other_subscriber, mut mia, mut moa, mut ref_other_claim, 
+         mut ref_rendering_provider, mut dtm_statement_from, mut dtm_coverage_expiration, mut dtm_claim_received, mut per, mut amt, mut qty);
 
     for _ in 0..clp_count {
         (clp, claim_adjustment, nm1_patient, nm1_insured, nm1_corrected_patient, nm1_service_provider, nm1_crossover_carrier, nm1_corrected_priority_payer,
@@ -108,16 +74,8 @@ fn main() {
     let svc_count= contents.matches("SVC").count();
     let mut loop_2110_array = vec![];
     println!("Number of loops in loop 2100: {:?}",svc_count);
-    let mut svc;
-    let mut dtm;
-    let mut cas;
-    let mut ref_service_identification;
-    let mut ref_line_item_control_number;
-    let mut ref_rendering_provider_information;
-    let mut ref_healthcare_policy_identification;
-    let mut amt;
-    let mut qty;
-    let mut lq;
+    let (mut svc, mut dtm, mut cas, mut ref_service_identification, mut ref_line_item_control_number, 
+         mut ref_rendering_provider_information, mut ref_healthcare_policy_identification, mut amt, mut qty, mut lq);
 
     for _ in 0..svc_count {
         (svc, dtm, cas, ref_service_identification, ref_line_item_control_number, ref_rendering_provider_information, ref_healthcare_policy_identification, amt, qty, lq, contents) =
