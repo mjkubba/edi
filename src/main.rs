d95143b850aa5d2c956aa56a835fdd082c0af471
MJ Kubba <mjkubba@gmail.com>
2024-02-10 20:11:55 -0600

    adding notes

--- src/main.rs
+++ src/main.rs
@@ -16,45 +16,11 @@ fn main() {
 
     /*
     TODO:
-        ~implement logger~
-        ~check if the file passed is 835, this can be read from ~ST*835*~
-        
-        make it safer when something does not exist
-        Check against the guide how many of each segment is in each loop, 
-        finding some mismatches between the standard and the implementation of EDI835!!!
-
-        Table 1: there are 3 PERs, 2 are optional and the required one may come in the middle
-
-        also noticed DTM is getting processed earlier, need to trim content based on loop segments and only send that part for processing.
-        Created a function: get_loop_content in helper takes the content and start and finish of the loop, then only provide that to the get_xyz
-        need to change the trimming and other thing already there, look at table1 as an example
-
-        The method I was going with is also unsafe, if you look at table 2, few of these loops and segements are all situational!
-        Need to rethink this
-        Maybe have a table of all the loops and segments and send the function the part we are looking for, eg table2-loop2000 and the 
-        function will return the content of that loop based on the table lookup.
-
-        check_if_segement_in_loop might need to be changed a bit or add another function to check against expected codes
-        eg DTM in loop table 1 comes with code 405 but not in the other loops
-        the current function is a simple way to check, but table 2 loop 2110 all situational and have no real anchor
-
-        I was warned this might be not good for my mental health but I never listened!!!!!
-        Another issue I just encountered, the segment repeats, I knew it exists but never noticed it in sample files
-        need more input!!!
-        an Idea here is to count the segment like DTM, see loop over all and assign variable based on the expected codes
-        mix of the idea of match in loop 1000a and check_for_expected_codes() in helper
-
+        refactor helper to keep generic thingies there and move specific 835 functions to another file or the controller.        
         Where I left:
-        in each loop, specifically the 2110 is having issues where the loops repeats, it will parse some of the segments from the 2nd loop ahead of time.
-        Need to trim loops and process one at a time.
-        2110 have a solution, looks weak to me, but best I can think of as of now, need to fix it and move it to helper or implement it as is in the other loops
-
-        another thingy, the loops are nested, so 2110 is part of 2100 and 2100 is part of 2000 so if 2100 is repeated then 2110 is inside each of 2100 repeat...
-        this mean I need to rewrite the controller to reflect that and maybe few otherparts of these loops.
-
-        Need to fix Write after figuring out the proper loops of table 2
-
-        Need to refactor loop2000 into table2
+        EDI835 is done, need to make sure if a supplied file is messed up or some weird input provided the program exit gracefully
+        also double check all the failure points
+        build unit test back again
 
 
     */
